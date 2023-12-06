b412b9935112f6cd9aea8898a6276e6172f3a931
bhoomi gaheja <vg199r@gmail.com>
2023-12-06 19:53:07 +0530

    first commit

--- jobspark/scrapping3.py
+++ jobspark/scrapping3.py
@@ -1,72 +1,51 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from jobspark.settings import CHROME_DRIVER_PATH
-from webdriver_manager.chrome import ChromeDriverManager
-import pathlib
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 from time import sleep
+import os
 
 def open_website():
-    
-    ScriptDir = pathlib.Path().absolute()
-    url = "https://flowgpt.com/chat"
-    driver_path = ChromeDriverManager().install()
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')  # Add any other options you need
 
-    service = webdriver.Chrome(service_args=["--verbose"], executable_path=driver_path, options=options)
+    ScriptDir = os.path.abspath(os.path.dirname(__file__))
+    user_data_dir = os.path.join(ScriptDir, 'chromedata')
+
     chrome_options = Options()
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"
     chrome_options.add_argument(f"user-agent={user_agent}")
-    chrome_options.add_argument(f"user-data-dir={ScriptDir}\\chromedata")
+    chrome_options.add_argument(f"user-data-dir={user_data_dir}")
+    chrome_options.add_argument('--headless')
 
+    # Use ChromeDriverManager to automatically download and manage ChromeDriver
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, options=chrome_options)
+
+    # Initialize Chrome driver with options
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.maximize_window()
-    driver.get(url=url)
+
     return driver
 
-ScriptDir = pathlib.Path().absolute()
-url = "https://flowgpt.com/chat"
-chrome_options = Options()
-
-# Set user agent
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"
-chrome_options.add_argument(f"user-agent={user_agent}")
-
-# Set custom user data directory (replace 'chromedata' with your desired folder name)
-chrome_options.add_argument(f"user-data-dir={ScriptDir}\\chromedata")
-chrome_options.add_argument('--headless')
-
-# Use ChromeDriverManager to automatically download and manage ChromeDriver
-service = Service(ChromeDriverManager().install())
-
-# Initialize Chrome driver with options
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.maximize_window()
-driver.get(url=url)
-
-# Avoid using long sleep times
-def website():
+def website(driver):
     while True:
-        try: 
+        try:
             Xpath = '/html/body/div[1]/main/div[3]/div/div[2]/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/textarea'
             driver.find_element(by=By.XPATH, value=Xpath)
             break
         except:
             pass
 
-def send(query):
+def send(driver, query):
     Xpath = '/html/body/div[1]/main/div[3]/div/div[2]/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/textarea'
     driver.find_element(by=By.XPATH, value=Xpath).send_keys(query)
     button_xpath = '/html/body/div[1]/main/div[3]/div/div[2]/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/button'
     driver.find_element(By.XPATH, value=button_xpath).click()
     sleep(10)  # Adjusted sleep time
 
-def text():
+def text(driver):
     soup = BeautifulSoup(driver.page_source, 'lxml')
     result = soup.find('div', class_='max-w-[62vw] md:w-full css-0')
 
@@ -76,27 +55,30 @@ def text():
     print(f"a:{scraped_text}")
     return scraped_text
 
-def popup():
+def popup(driver):
     try:
         # Find and click the element to close the popup
         xpath = '/html/body/div[1]/main/div[3]/div/div[2]/div/div[3]/div[2]/div/div[2]/div[2]/div[1]/button'
         driver.find_element(by=By.XPATH, value=xpath).click()
     except Exception as e:
         print('Exception occurred during popup closing:', str(e))
-   
-      
+
 def gpt(query):
-    website()
-    send(query)
-    scraped_text = text()  # Assuming text() returns the scraped text as a list
+    driver = open_website()
+    website(driver)
+    send(driver, query)
+    scraped_text = text(driver)  # Assuming text() returns the scraped text as a list
     sleep(5)  
-    popup()
+    popup(driver)
     # Return the scraped data as a dictionary
     scraped_data = {'scraped_text': scraped_text}
     print(f"b: {scraped_data}")
+
+    # Close the driver
+    driver.quit()
     
     return scraped_data
-    
 
 # Example usage
-
+gpt_result = gpt('your_query_here')
+print(gpt_result)
