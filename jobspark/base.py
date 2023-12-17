5edf5dc9810ff192678d3642337c40b157c99cfe
bhoomi gaheja <vg199r@gmail.com>
2023-12-17 19:35:50 +0530

    first commit

--- jobspark/base.py
+++
@@ -1,9 +0,0 @@
-from .models import User
-
-def authenticate_user(email):
-    try:
-        user = User.objects.get(email=email)
-        return user
-    except User.DoesNotExist:
-        return None
-
