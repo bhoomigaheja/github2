f10aa0c016d49a8b67bd0ac735026adbb95ab91e
bhoomi gaheja <vg199r@gmail.com>
2023-12-18 15:12:24 +0530

    first commit

--- jobspark/settings.py
+++ jobspark/settings.py
@@ -23,7 +23,7 @@ import os
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("12wertykli323456fhjkpoiue45678909876543")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","FALSE").lower()== "true"
