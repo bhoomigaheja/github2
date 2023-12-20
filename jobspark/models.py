528e3832f485ca4b4735b9fcb21236362f74d178
bhoomi gaheja <vg199r@gmail.com>
2023-12-20 10:27:16 +0530

    first commit

--- jobspark/models.py
+++ jobspark/models.py
@@ -85,4 +85,14 @@ class User(models.Model):
     last_name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     repeat_password=models.CharField(max_length=255)
-    
+
+
+# In models.py
+from django.db import models
+
+class JobApplication(models.Model):
+    job_title = models.CharField(max_length=255)
+    cover_letter = models.TextField()
+
+    def __str__(self):
+        return self.job_title
