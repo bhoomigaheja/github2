8ec96d75b84feff84c947c328c765358270fb7f2
bhoomi gaheja <vg199r@gmail.com>
2023-12-20 12:15:06 +0530

    first commit

--- jobspark/views.py
+++ jobspark/views.py
@@ -93,8 +93,8 @@ def login_view(request):
 
 def naukri_view2(request):
     query = request.POST.get('query2', '')
-
-    return render(request, 'jobs2.html', {'query2': query})
+    modified_query = query.replace(' ', '-')
+    return render(request, 'jobs2.html', {'query2': query, 'modified_query': modified_query})
 
 
 def naukri_view(request):
